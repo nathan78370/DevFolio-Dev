830a508d91dfd665bc2bd6ecfa65ec69734bf9d8
LibertyGang <49068085+nathan78370@users.noreply.github.com>
2021-03-05 16:41:08 +0100

    Changement en parti en PHP

--- index.php
+++ index.php
@@ -1,70 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
-    <title>PortFolio</title>
-    <meta content="" name="description" />
-    <meta content="" name="keywords" />
-
-    <!-- Favicons --> 
-    <link href="assets/img/logo.png" rel="icon" />
-    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon" />
-
-    <!-- Vendor CSS Files DE-->
-    <link
-      href="assets/vendor/bootstrap/css/bootstrap.min.css"
-      rel="stylesheet"
-    />
-    <link
-      href="assets/vendor/bootstrap-icons/bootstrap-icons.css"
-      rel="stylesheet"
-    />
-    <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet"/>
-    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet" />
-
-    <!-- Template Main CSS File -->
-    <link href="assets/css/style.css" rel="stylesheet" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
-    
-  </head>
-
+<?php
+//Cette fonction doit être appelée avant tout code html
+session_start();
+include("assets/includes/menu.php");
+include("assets/includes/link.php");
+?>
   <body>
-    <!-- ======= Header ======= -->
-    <header id="header" class="fixed-top">
-      <div class="container d-flex align-items-center justify-content-between">
-        <h1 class="logo"><a style="text-decoration:none" href="index.html#fond">PortFolio</a></h1>
-        <!-- Uncomment below if you prefer to use an image logo -->
-        <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
-
-        <nav id="navbar" class="navbar">
-          <ul>
-            <li><a class="nav-link scrollto active" href="#fond">Home</a></li>
-            <li><a class="nav-link scrollto" href="#about">Présentation</a></li>
-            <!--<li><a class="nav-link scrollto" href="#services">Services</a></li>-->
-            <li><a class="nav-link scrollto" href="#work">PortFolio</a></li>
-            <li>
-              <div class="container-fluid">
-                <form class="d-flex">
-                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                    <button class="btn btn-outline-success" type="submit"><i class="bi bi-search"></i>
-                    </button>
-                  </input>
-                </form>
-              </div>
-            </li>
-            <li><a class="nav-link scrollto" href="login.html">Connexion</a></li>
-            <!--<li><a class="nav-link scrollto" href="#blog">Blog</a></li>-->
-            <!--<li><a class="nav-link scrollto" href="#contact">Contact</a></li>-->
-          </ul>
-          <i class="bi bi-list mobile-nav-toggle"></i>
-        </nav>
-        <!-- .navbar -->
-      </div>
-    </header>
-    <!-- End Header -->
 
     <!-- ======= Hero Section ======= -->
     <div
