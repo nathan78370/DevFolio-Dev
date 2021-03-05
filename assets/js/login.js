454b97882563bc690d627e84b57b5b703c4c2e15
LibertyGang <49068085+nathan78370@users.noreply.github.com>
2021-03-05 01:44:40 +0100

    Modification page connexion

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,60 +1,157 @@
-$(document).ready(function () {
-  var animating = false,
-    submitPhase1 = 1100,
-    submitPhase2 = 400,
-    logoutPhase1 = 800,
-    $login = $(".login"),
-    $app = $(".app");
+var pwd = document.getElementById("pwd");
+var eye = document.getElementById("eye");
 
-  function ripple(elem, e) {
-    $(".ripple").remove();
-    var elTop = elem.offset().top,
-      elLeft = elem.offset().left,
-      x = e.pageX - elLeft,
-      y = e.pageY - elTop;
-    var $ripple = $("<div class='ripple'></div>");
-    $ripple.css({ top: y, left: x });
-    elem.append($ripple);
+eye.addEventListener("click", togglePass);
+
+function togglePass() {
+  eye.classList.toggle("active");
+
+  pwd.type == "password" ? (pwd.type = "text") : (pwd.type = "password");
+}
+
+// Form Validation
+
+function checkStuff() {
+  var email = document.form1.email;
+  var password = document.form1.password;
+  var msg = document.getElementById("msg");
+
+  if (email.value == "") {
+    msg.style.display = "block";
+    msg.innerHTML = "Please enter your email";
+    email.focus();
+    return false;
+  } else {
+    msg.innerHTML = "";
   }
 
-  $(document).on("click", ".login__submit", function (e) {
-    if (animating) return;
-    animating = true;
-    var that = this;
-    ripple($(that), e);
-    $(that).addClass("processing");
-    setTimeout(function () {
-      $(that).addClass("success");
-      setTimeout(function () {
-        $app.show();
-        $app.css("top");
-        $app.addClass("active");
-      }, submitPhase2 - 70);
-      setTimeout(function () {
-        $login.hide();
-        $login.addClass("inactive");
-        animating = false;
-        $(that).removeClass("success processing");
-      }, submitPhase2);
-    }, submitPhase1);
-  });
+  if (password.value == "") {
+    msg.innerHTML = "Please enter your password";
+    password.focus();
+    return false;
+  } else {
+    msg.innerHTML = "";
+  }
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!re.test(email.value)) {
+    msg.innerHTML = "Please enter a valid email";
+    email.focus();
+    return false;
+  } else {
+    msg.innerHTML = "";
+  }
+}
 
-  $(document).on("click", ".app__logout", function (e) {
-    if (animating) return;
-    $(".ripple").remove();
-    animating = true;
-    var that = this;
-    $(that).addClass("clicked");
-    setTimeout(function () {
-      $app.removeClass("active");
-      $login.show();
-      $login.css("top");
-      $login.removeClass("inactive");
-    }, logoutPhase1 - 120);
-    setTimeout(function () {
-      $app.hide();
-      animating = false;
-      $(that).removeClass("clicked");
-    }, logoutPhase1);
-  });
+// ParticlesJS
+
+// ParticlesJS Config.
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 60,
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+    },
+    color: {
+      value: "#ffffff",
+    },
+    shape: {
+      type: "circle",
+      stroke: {
+        width: 0,
+        color: "#000000",
+      },
+      polygon: {
+        nb_sides: 5,
+      },
+      image: {
+        src: "img/github.svg",
+        width: 100,
+        height: 100,
+      },
+    },
+    opacity: {
+      value: 0.1,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 1,
+        opacity_min: 0.1,
+        sync: false,
+      },
+    },
+    size: {
+      value: 6,
+      random: false,
+      anim: {
+        enable: false,
+        speed: 40,
+        size_min: 0.1,
+        sync: false,
+      },
+    },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#ffffff",
+      opacity: 0.1,
+      width: 2,
+    },
+    move: {
+      enable: true,
+      speed: 1.5,
+      direction: "top",
+      random: false,
+      straight: false,
+      out_mode: "out",
+      bounce: false,
+      attract: {
+        enable: false,
+        rotateX: 600,
+        rotateY: 1200,
+      },
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false,
+        mode: "repulse",
+      },
+      onclick: {
+        enable: false,
+        mode: "push",
+      },
+      resize: true,
+    },
+    modes: {
+      grab: {
+        distance: 400,
+        line_linked: {
+          opacity: 1,
+        },
+      },
+      bubble: {
+        distance: 400,
+        size: 40,
+        duration: 2,
+        opacity: 8,
+        speed: 3,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+      push: {
+        particles_nb: 4,
+      },
+      remove: {
+        particles_nb: 2,
+      },
+    },
+  },
+  retina_detect: true,
 });
